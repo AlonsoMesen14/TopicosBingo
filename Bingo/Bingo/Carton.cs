407a6cea943615bc45b860bbb95f77bd28bf3f9c
AlonsoMesen14 <totomesen@hotmail.com>
2019-02-05 18:43:32 -0600

    carton
    se agrego la clase carton

--- Bingo/Bingo/Carton.cs
+++ Bingo/Bingo/Carton.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
 namespace Bingo
 {
     class Carton
     {
+        private bool haGanado { get; set; }
+        private object [,] carton { get; set; }
+
+        public Carton(bool haGanado, object[,] carton)
+        {
+            this.haGanado = haGanado;
+            this.carton = carton;
+        }
+
+
     }
 }
