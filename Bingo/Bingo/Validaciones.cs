4ced1174b71f7170e68e79a94342431852cb8c9f
Oxnock <jsezuniga72@gmail.com>
2019-02-07 18:59:47 -0600

    Optimizacion Validaciones
    Aaron se la come :vvvvv

--- Bingo/Bingo/Validaciones.cs
+++ Bingo/Bingo/Validaciones.cs
@@ -11,16 +11,7 @@ namespace Bingo
     {
         static bool Ganador = false;
         WcfServicio.WCFBingoClient WCFInstancia = new WcfServicio.WCFBingoClient();
-        public void ValidacionCartonLLeno()
-        {
 
-        }
-
-        public void Validar4esquinas()
-        {
-
-        }
-        //este peerrrrrooooooooooooooooooooooo
         public static void Revision(int Tipo) {
             for (int i = 0; i < Program.Players.Count; i++)
             {
@@ -44,76 +35,32 @@ namespace Bingo
             object [,]carton = y.cartones[posicion].getCartonActual();
             switch (tipo)
             {
-
                 case 1://cuatro esquinas
-                    numerosTiene.Add(carton[1,0]);
-                    numerosTiene.Add(carton[1, 4]);
-                    numerosTiene.Add(carton[5, 0]);
-                    numerosTiene.Add(carton[5, 4]);
+                    numerosTiene = CuatroEsquina(numerosTiene,carton);
                     break;
                 case 2://letra h
-                    for (int i = 1; i <= 5; i++)
-                    {
-                        numerosTiene.Add(carton[i, 0]);
-                        numerosTiene.Add(carton[i, 4]);
-                    }
-                    numerosTiene.Add(carton[3, 1]);
-                    numerosTiene.Add(carton[3, 3]);
-
+                    numerosTiene = LetraH(numerosTiene, carton);
                     break;
                 case 3:// letra x
-                    numerosTiene.Add(carton[1, 0]);
-                    numerosTiene.Add(carton[2, 1]);
-                    numerosTiene.Add(carton[4, 3]);
-                    numerosTiene.Add(carton[5, 4]);
-                    numerosTiene.Add(carton[1, 4]);
-                    numerosTiene.Add(carton[2, 3]);
-                    numerosTiene.Add(carton[4, 1]);
-                    numerosTiene.Add(carton[5, 0]);
+                    numerosTiene = LetraX(numerosTiene, carton);
                     break;
                 case 4://letra o
-                    for (int i = 0; i < 5; i++)
-                    {
-                        numerosTiene.Add(carton[1, i]);
-                        numerosTiene.Add(carton[5, i]);
-                    }
-
-                    for (int i = 2; i < 5; i++)
-                    {
-                        numerosTiene.Add(carton[i, 0]);
-                        numerosTiene.Add(carton[i, 4]);
-                    }
-
+                    numerosTiene = LetraO(numerosTiene, carton);
                     break;
                 case 5://carton lleno
-                    for (int f = 0; f < carton.GetLength(0); f++)
-                    {
-                        for (int c = 0; c < carton.GetLength(1); c++)
-                        {
-                            numerosTiene.Add(carton[f, c]);
-                        }
-                    }
-                    numerosTiene.RemoveAt(12);
+                    numerosTiene = CartonLleno(numerosTiene, carton);
                     break;
-
-
-
             }
-            numerosTiene.ToString();
             if (YaGano(numerosTiene) == true){
                 Console.WriteLine("Ya tenemos ganador: "+y.NombreUsuario());
                 Console.WriteLine("Con el carton");
                 y.cartones[posicion].Imprimir();
                 Ganador = true;
             }
-           
-
         }
-
     
         public static bool YaGano(ArrayList numerosTiene)
         {
-
             for (int i = 0; i < Program.BINGO.Count; i++)
             {
                 for (int j = 0; j < numerosTiene.Count; j++)
@@ -121,7 +68,6 @@ namespace Bingo
                     if (numerosTiene[j].Equals(Program.BINGO[i]))
                     {
                         numerosTiene.RemoveAt(j);
-                        //j++;
                     }
                     if (numerosTiene.Count == 0)
                     {
@@ -134,16 +80,68 @@ namespace Bingo
                     {
                      return true;
                     }
-
                     return false;
         }
 
+        public static ArrayList CuatroEsquina(ArrayList numerosTiene, object[,] carton) {
+            numerosTiene.Add(carton[1, 0]);
+            numerosTiene.Add(carton[1, 4]);
+            numerosTiene.Add(carton[5, 0]);
+            numerosTiene.Add(carton[5, 4]);
+            return numerosTiene;
+        }
 
+        public static ArrayList LetraH(ArrayList numerosTiene, object[,] carton)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                numerosTiene.Add(carton[i, 0]);
+                numerosTiene.Add(carton[i, 4]);
+            }
+            numerosTiene.Add(carton[3, 1]);
+            numerosTiene.Add(carton[3, 3]);
 
+            return numerosTiene;
+        }
+        public static ArrayList LetraX(ArrayList numerosTiene, object[,] carton)
+        {
+            numerosTiene.Add(carton[1, 0]);
+            numerosTiene.Add(carton[2, 1]);
+            numerosTiene.Add(carton[4, 3]);
+            numerosTiene.Add(carton[5, 4]);
+            numerosTiene.Add(carton[1, 4]);
+            numerosTiene.Add(carton[2, 3]);
+            numerosTiene.Add(carton[4, 1]);
+            numerosTiene.Add(carton[5, 0]);
+            return numerosTiene;
+        }
+        public static ArrayList LetraO(ArrayList numerosTiene, object[,] carton)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                numerosTiene.Add(carton[1, i]);
+                numerosTiene.Add(carton[5, i]);
+            }
 
-
-
-
+            for (int i = 2; i < 5; i++)
+            {
+                numerosTiene.Add(carton[i, 0]);
+                numerosTiene.Add(carton[i, 4]);
+            }
+            return numerosTiene;
+        }
+        public static ArrayList CartonLleno(ArrayList numerosTiene, object[,] carton)
+        {
+            for (int f = 0; f < carton.GetLength(0); f++)
+            {
+                for (int c = 0; c < carton.GetLength(1); c++)
+                {
+                    numerosTiene.Add(carton[f, c]);
+                }
+            }
+            numerosTiene.RemoveAt(12);
+            return numerosTiene;
+        }
 
 
 
